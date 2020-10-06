c5fe8540f06a3f778a4894d13bfc9f48f6a5308b
David Brookton <david.zimmerman@colorado.edu>
2020-10-06 17:33:40 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,9 +2,9 @@ export function manageFriends(state, action){
   switch (action.type) {
   case 'ADD_FRIEND':
         friend: {
-        name: "Chrome Boi"
-        homewtown: "NYC",
-        id: 1
+        name: action.name,
+        homewtown: action.hometown,
+        id: action.id
       },
     return {friends: state.friends ...friend}
   case 'REMOVE_FRIEND':
